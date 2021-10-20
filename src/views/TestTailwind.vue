8f5815949d066387e0b7ae819d1dfe0cb15c1a06
Norlix <75667735+Norlix@users.noreply.github.com>
2021-10-20 11:26:51 +0200

    poc-Tailwind-Test-added

--- src/views/TestTailwind.vue
+++ src/views/TestTailwind.vue
@@ -1,14 +1,20 @@
 <template>
 <div>
     <h1>Test für TailwindCSS</h1>
-    <div class="test tw-shadow-2xl tw-bg-red-600">Hallo</div>
-    <h2>Halloho<h2>
+    <div class="test tw-shadow-2xl bg-hauptfarbe tw-absolute tw-text-center tw-rounded-full tw-left-5 tw-top-5 tx-yellow">{{string}}</div>
+    <div class="test tw-shadow-12x4 hauptfarbe tw-absolute tw-right-5 tw-top-5"><img src="/img/icons/cupboard_logo.png" alt="Error"> </div>
 </div>  
+
+
 </template>
 
 <script>
 export default {
-
+data: function () {
+        return {
+            string: "Closing</Tag>"
+        }
+    }
 }
 </script>
 
@@ -21,6 +27,15 @@ h1 {
     height: 10vw;
     width: 10vw;
     margin: 0 auto;
+}
+.tx-hauptfarbe{
+    color: #012840
+}
+.bg-hauptfarbe{
+    background-color: #012840;
+}
+.tx-yellow{
+    color:  #8C8A0A
 }
 </style>
 
