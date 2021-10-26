81dadf1515af8ca1cfc238820db0eef842b8f85d
Jan Moormann <jan@moormann-family.de>
2021-10-26 23:44:59 +0200

    get products from api

--- frontend/src/components/Products/ProductInformation.vue
+++ frontend/src/components/Products/ProductInformation.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="tw-grid tw-grid-cols-3 lg:tw-grid-cols-8 tw-gap-4 tw-overflow-hidden" style="background-color: light-grey">
     <div class="tw-col-span-3 lg:tw-col-span-2 lg:tw-pl-5">
-        <img class="tw-object-contain tw-object-center lg:tw-max-h-80" src="https://s3.eu-gb.cloud-object-storage.appdomain.cloud/cupboardimages/luso-highboard-a073367-001-11.jpg">
+        <img class="tw-object-contain tw-object-center lg:tw-max-h-80" :src="imageUrl">
     </div>
     <div class="tw-col-span-3 tw-p-10">
-        <h3 class="tw-text-3xl tw-text-center lg:tw-text-left">Schrank</h3>
+        <h3 class="tw-text-3xl tw-text-center lg:tw-text-left">{{categorieName}}</h3>
         <br>
-        <p class="tw-text-lg tw-text-center lg:tw-text-left">Produktbeschreibung:</p>
-        <p class="tw-text-center lg:tw-text-left">Schönes Lowboard<br>
-        aus Tannenholz<br>
-        mit Schublade
+        <p class="tw-text-lg tw-font-bold tw-text-center lg:tw-text-left">Produktbeschreibung:</p>
+        <p class="tw-text-center lg:tw-text-left">Schönes {{categorieName}}<br>
+        aus nachahltigem Holz gefertigt<br>
+        mit hochwertigen Schubladen
         </p>
     </div>
     <div class="tw-col-span-3 tw-h-52 lg:tw-h-full tw-p-10">
-        <h3 class="tw-text-3xl tw-text-center lg:tw-text-right tw-font-bold">ab 100,50€</h3>
+        <h3 class="tw-text-3xl tw-text-center lg:tw-text-right tw-font-bold">{{price}}</h3>
         <div class="tw-h-full tw-w-full tw-relative">
-          <button class="tw-bg-blue hover:tw-bg-primary tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded tw-absolute tw-bottom-10 tw-right-1/2 tw-transform tw-translate-x-1/2 lg:tw-translate-x-0 lg:tw-right-0">Zum Warenkorb hinzufügen</button>
-          <button class="tw-bg-blue hover:tw-bg-primary tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded tw-absolute tw-bottom-10 tw-right-1/2 tw-transform tw-translate-x-1/2 lg:tw-translate-x-0 lg:tw-right-0">Konfigurieren</button>
+          <button v-if="customizable" class="tw-bg-blue hover:tw-bg-primary tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded tw-absolute tw-bottom-10 tw-right-1/2 tw-transform tw-translate-x-1/2 lg:tw-translate-x-0 lg:tw-right-0">Konfigurieren</button>
+          <button v-else class="tw-bg-blue hover:tw-bg-primary tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded tw-absolute tw-bottom-10 tw-right-1/2 tw-transform tw-translate-x-1/2 lg:tw-translate-x-0 lg:tw-right-0">Zum Warenkorb hinzufügen</button>
+          
         </div>
     </div>
   </div>
@@ -24,6 +25,30 @@
 
 <script>
 export default {
+  name: 'ProductInfotmation',
+  props: {
+      //Props for Lottie Web Player
+      categorieName: {
+        type: String,
+        required: true
+      },
+      imageUrl: {
+        type: String,
+        required: true
+      },
+      price:{
+        type: String,
+        required: true
+      },
+      customizable: {
+        type: Boolean,
+        required: true
+      },
+      priceStarting:{
+        type: Boolean,
+        required: true
+      }
+  }
 
 }
 </script>
--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="tw-mt-20">
     <div class="tw-grid tw-grid-cols-1 tw-divide-y tw-divide-blue">
-      <ProductInformation />
-      <ProductInformation />
-      <ProductInformation />
+      <ProductInformation 
+       v-for="product in products" :key="product.id"
+      :categorieName='product.name'
+      :imageUrl='product.imageUrl'
+      :price='product.price'
+      :customizable='product.customizable'
+      :priceStarting='product.priceStarting'
+      />
     </div>
-  </div>
-  <div class="tw-w-full tw-min-h-40 tw-bg-green-100">
-    <ul>
-      <li>Die folgenden Kategorien werden aus dem Backend geholt: (Dafür muss das Backend auch ausgeführt werden!)</li>
-      <li v-for="categorie in categories" :key="categorie.id">{{ categorie.name }}</li>
-    </ul>
   </div>
 </template>
 
@@ -24,13 +23,19 @@ export default {
     },
     data() {
       return {
-        categories: []
+        categories: [],
+        products: []
       }
     },
     async mounted() {
-        const { data } = await this.axios.get('catalogue/categories');
+        const {data} = await this.axios.get('catalogue/categories');
         this.categories = data;
         console.log(this.categories)
+        for(const categorie of this.categories) {
+          const {data} = await this.axios.get('catalogue/categories/' + categorie.id + '/products')
+          this.products.push(data[0]);
+          console.log(this.products)
+        }
     },
         
 };
