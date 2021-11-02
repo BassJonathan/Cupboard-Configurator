aca500bd67dfee583e54e7aede1d1f94c6dd1ddf
BassJonathan <44204074+BassJonathan@users.noreply.github.com>
2021-11-02 23:21:49 +0100

    Home-page animation updated

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -56,71 +56,156 @@
     <div class="tw-relative tw-h-10"></div>
     <Sustainability />
     <div class="tw-w-full">
-      <p class="tw-text-3xl tw-p-8 heading_font">
-        Konfiguriere deinen individuellen Schrank in drei einfachen Schritten:
+      <p class="tw-text-4xl tw-font-extrabold tw-p-8 heading_font">
+        Probiere jetzt unseren innovativen 3-Schritt-Konfigurator aus:
       </p>
     </div>
-    <div class="tw-w-full">
-      <div
-        class="
-          tw-w-full tw-grid
-          md:tw-grid-rows-3 md:tw-grid-cols-2
-          tw-gap-6 tw-grid-rows-6 tw-grid-cols-1
-        "
-      >
-        <div class="md:tw-order-1 tw-order-2">Step1</div>
-        <div class="md:tw-order-2 tw-order-1">
+    <div class="tw-w-full tw-h-auto tw-flex tw-justify-center">
+      <div class="tw-w-1/2" id="sticky-container">
+        <div class="tw-sticky tw-top-16">
           <InterAnim
             playerID="anim_size"
-            playerSrc="01_Choose_Size.json"
-            playerClass="full"
-            playerContainer="anim1"
-            playerSpeed="0.5"
+            playerSrc="animation.json"
+            interactivityContainer="sticky-container"
             :interactivityActions="[
-              { visibility: [0, 0.2], type: 'stop', frames: [0] },
-              { visibility: [0.2, 0.6], type: 'seek', frames: [0, 111] },
-              { visibilty: [0.6, 1], type: 'stop', frames: [112] },
-            ]"
-          />
-        </div>
-        <div class="tw-order-3">
-          <InterAnim
-            playerID="anim_fittings"
-            playerSrc="02_Choose_Fittings.json"
-            playerClass="full"
-            playerContainer="anim2"
-            playerSpeed="0.5"
-            :interactivityActions="[
-              { visibility: [0, 0.2], type: 'stop', frames: [0] },
-              { visibility: [0.2, 0.6], type: 'seek', frames: [0, 111] },
-              { visibilty: [0.6, 1], type: 'stop', frames: [112] },
-            ]"
-          />
-        </div>
-        <div class="tw-order-4">Step2</div>
-        <div class="md:tw-order-5 tw-order-6">Step3</div>
-        <div class="md:tw-order-6 tw-order-5">
-          <InterAnim
-            playerID="anim_material"
-            playerSrc="03_Choose_Material.json"
-            playerClass="full"
-            playerContainer="anim3"
-            playerSpeed="0.5"
-            :interactivityActions="[
-              { visibility: [0, 0.2], type: 'stop', frames: [0] },
-              { visibility: [0.2, 0.8], type: 'seek', frames: [0, 6] },
-              { visibilty: [0.8, 1], type: 'stop', frames: [6] },
+              { visibility: [0, 0.25], type: 'stop', frames: [0] },
+              { visibility: [0.25, 0.7], type: 'seek', frames: [0, 333] },
+              { visibilty: [0.7, 1], type: 'stop', frames: [333] },
             ]"
           />
         </div>
       </div>
+      <div class="tw-w-1/2 tw-pl-20">
+        <div style="height: 200px"></div>
+        <div class="tw-w-full tw-p-5">
+            <div class="tw-w-full tw-pb-1 tw-text-5xl tw-font-extrabold tw-text-transparent tw-bg-gradient-to-br tw-from-primary-gradient2 tw-to-primary-gradient4 tw-bg-clip-text">
+                Schritt 1: Bestimme die Maße
+            </div>
+            <div class="tw-w-full tw-text-3xl tw-text-gray-500 tw-pl-4">
+                Damit dein Schrank genau passt!
+            </div>
+            <div class="tw-w-full ">
+              <div class="tw-text-xl tw-pl-2 tw-pr-14 tw-py-4">
+                Im ersten Schritt kannst du die Maße von deinem Schrank auf den Centimeter genau bestimmen. Mit unseren hochleistungs-präzisions Sägen können wir Teile für einen Schrank von bis zu 3m Höhe und einer maximalen Breite von 5m fertigen. Unser Holz stammt zu 100% aus FSC-Zertizierten Wäldern und für jeden gefällten Baum wird ein neuer geplanzt.
+                Wenn du einen größeren Schrank bestellen möchtest, kannst du uns auch gerne über unser Kontaktformular eine Nachricht schreiben.
+              </div>
+              <div class="tw-w-full tw-flex tw-justify-right">
+                <div class="tw-flex w-justify-start tw-items-center tw-pr-8 tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/quality.png" alt="Height" class="tw-object-cover tw-p-3 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Max. Höhe 3m
+                  </div>
+                </div>
+                <div class="tw-flex w-justify-start tw-items-center tw-pr-8 tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/height.png" alt="Height" class="tw-object-cover tw-p-3 tw-filter tw-invert tw-transform tw-rotate-90">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Max. Breite 5m
+                  </div>
+                </div>
+                <div class="tw-flex w-justify-start tw-items-center tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/laser.png" alt="Height" class="tw-object-cover tw-p-3 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Laser-Schnitt
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div style="height:300px"></div>
+        <div class="tw-w-full tw-p-5">
+            <div class="tw-w-full tw-pb-1 tw-text-5xl tw-font-extrabold tw-text-transparent tw-bg-gradient-to-br tw-from-primary-gradient2 tw-to-primary-gradient4 tw-bg-clip-text">
+                Schritt 2: Wähle deine Austattung
+            </div>
+            <div class="tw-w-full tw-text-3xl tw-text-gray-500 tw-pl-4">
+                Genau für deine Bedürfnisse!
+            </div>
+            <div class="tw-w-full ">
+              <div class="tw-text-xl tw-pl-2 tw-pr-14 tw-py-4">
+                Als Zweites kannst du deine individuelle Ausstattung auswählen. Wir bieten eine Vielzahl an Auswahlmöglichkeiten, wodurch du deinen Schrank genau für deinen Zweck anpassen kannst. Ob Kleiderschrank oder Vitrine - alles ist möglich. Dabei kannst du zwischen einfachen Regalbrettern, Kleiderstangen, Drahtkörben, Schubladen und Lampen auswählen.
+                Alle Gegenstände sind aus unserem zertifiziertem Holz oder werden hochqualitativ in Europa zu fairen Löhen und guten Arbeitsbedingungen hergestellt.
+              </div>
+              <div class="tw-w-full tw-flex tw-justify-right">
+                <div class="tw-flex w-justify-start tw-items-center tw-pr-8 tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/quality.png" alt="Quality" class="tw-object-cover tw-p-2 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Höchste Qualität
+                  </div>
+                </div>
+                <div class="tw-flex w-justify-start tw-items-center tw-pr-8 tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/customization.png" alt="Customization" class="tw-object-cover tw-p-2 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Individuell
+                  </div>
+                </div>
+                <div class="tw-flex tw-justify-start tw-items-center tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/fair.png" alt="Height" class="tw-object-cover tw-p-3 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Fair
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div style="height:300px"></div>
+        <div class="tw-w-full tw-p-5">
+            <div class="tw-w-full tw-pb-1 tw-text-5xl tw-font-extrabold tw-text-transparent tw-bg-gradient-to-br tw-from-primary-gradient2 tw-to-primary-gradient4 tw-bg-clip-text">
+                Schritt 3: Wähle das Holz
+            </div>
+            <div class="tw-w-full tw-text-3xl tw-text-gray-500 tw-pl-4">
+                Harmoniert mit jeder Einrichtung!
+            </div>
+            <div class="tw-w-full ">
+              <div class="tw-text-xl tw-pl-2 tw-pr-14 tw-py-4">
+                Der letzte Schritt stellt die Auswahl des Holzes dar. Wir bieten neben der klassichen Weißen Ausführung auch weitere Holzarten für dein Regal an. Ahorn verleiht dem Schrank eine warme, edele Farbe und bietet von Natur aus eine sehr hohe Härte. Walnuss passt durch die nah Scharze färbung optimal zu dem modernen Industial-Look und ergänzt sich sehr gut mit goldener Deko. Im Kontrast dazu kann ein helles Regal aus Birke in großen, lichtdurchfluteten Räumen die perfekte Ergänzung darstellen.
+                Das gesamte Holz stammt aus kontrollierten und nachhaltig bewirtschafteten Wäldern.
+              </div>
+              <div class="tw-w-full tw-flex tw-justify-right">
+                <div class="tw-flex w-justify-start tw-items-center tw-pr-8 tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/environment.png" alt="Quality" class="tw-object-cover tw-p-2 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Umweltfreundlich
+                  </div>
+                </div>
+                <div class="tw-flex w-justify-start tw-items-center tw-pr-8 tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/forest.png" alt="Customization" class="tw-object-cover tw-p-3 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Aufforstung
+                  </div>
+                </div>
+                <div class="tw-flex tw-justify-start tw-items-center tw-w-1/3">
+                  <div class="tw-rounded-full tw-bg-gradient-to-b tw-from-green-gradient2 tw-to-green-gradient3 tw-w-14 tw-h-14">
+                    <img src="/img/home/co2.png" alt="Height" class="tw-object-cover tw-p-3 tw-filter tw-invert">
+                  </div>
+                  <div class="tw-text-lg tw-pl-3">
+                    Klimaneutral
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div style="height: 200px"></div>
+      </div>
     </div>
-    <div class="tw-w-full">
-      <p class="tw-text-3xl tw-p-8 heading_font">
-        Hier direkt zum Konfigurator: (BUTTON)
-      </p>
+    <div class="tw-w-full tw-flex tw-align-middle tw-justify-center tw-py-16 tw-bg-gray-200 tw-shadow-inner">
+      <div class="tw-text-3xl">Hier direkt zu unserem Konfigurator: </div>
+        <router-link class="btn btn-primary tw-ml-4 tw-text-xl tw-transition tw-duration-700 tw-ease-in-out tw-bg-gradient-to-tr tw-from-primary-gradient3 tw-to-primary-gradient5 hover:tw-from-primary-gradient4 hover:tw-to-primary-gradient6" to="/configurator">Zum Konfigurator</router-link>
     </div>
-    <div style="height: 1000px"></div>
   </div>
 </template>
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,13 +14,29 @@ module.exports = {
         primary: {
           DEFAULT: "#012840",
           gradient1: "#003352",
-          gradient2: "#002033",
+          gradient2: "#003a5c",
+          gradient3: "#005080",
+          gradient4: "#0066a3",
+          gradient5: "#007dc7",
+          gradient6: "#00a0ff"
         },
         blue: "#115D8C",
-        red: "#400808",
+        red: {
+          DEFAULT: "#400808",
+          gradient1: "#690a0a",
+          gradient2: "#8f0b0b",
+          gradient3: "#b30c0c",
+          gradient4: "#e00b0b",
+          gradient5: "#fc0303"
+        },
         green: {
-          DEFAULT: "#403F08",
-          dark: "#8C8A0A",
+          DEFAULT: "#8C8A0A",
+          dark: "#403F08",
+          gradient1: "#b3b00b",
+          gradient2: "#c7c40a",
+          gradient3: "#e3e009",
+          gradient4: "#f5f102"
+          
         },
       },
       minHeight: (theme) => ({
