ffbbaefcbf8bd794063605046e734217b347fe9d
sobu02 <75607259+sobu02@users.noreply.github.com>
2021-11-03 10:12:38 +0100

    Translation Imprint

--- frontend/src/views/Imprint.vue
+++ frontend/src/views/Imprint.vue
@@ -23,7 +23,7 @@
             tx-weiss
           "
         >
-          <br /><br />Impressum
+          <br /><br />{{ t("Imprint.start") }}
         </div>
         <div
           class="
@@ -34,8 +34,9 @@
         >
           <div class="ml-8 tw-flex tw-flex-col tw-justify-start">
             <div class="ml-8 tw-flex tw-flex-col tw-gap-4">
-              <p class="tw-text-3xl tw-font-bold tx-weiss">Herausgeber</p>
+              <p class="tw-text-3xl tw-font-bold tx-weiss">{{ t("Imprint.editor") }}</p>
               <p class="tw-text-lg tx-weiss">
+                <!--{{ t("Imprint.editortext") }}-->
                 dieser Website und verantwortlich für den Inhalt gemäß §5 TMG
                 und § 55 RStV ist:<br />
                 Cupboard Inc.<br />
@@ -57,61 +58,31 @@
             </div>
             <div class="ml-8 tw-flex tw-flex-col tw-gap-4">
               <p class="tw-text-3xl tw-font-bold tx-weiss">
-                Haftungsausschluss (Disclaimer)
+                {{ t("Imprint.disclaimer") }}
               </p>
               <p class="tw-text-lg tx-weiss">
-                Als Dienstanbieter und Verkäufer sind wir gemäß § 7 Abs.1 TMG
-                für eigene Inhalte auf diesen Seiten nach den allgemeinen
-                Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als
-                Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
-                gespeicherte fremde Informationen zu überwachen oder nach
-                Umständen zu forschen, die auf eine rechtswidrige Tätigkeit
-                hinweisen. Verpflichtungen zur Entfernung oder Sperrung der
-                Nutzung von Informationen nach den allgemeinen Gesetzen bleiben
-                hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst
-                ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung
-                möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen
-                werden wir diese Inhalte umgehend entfernen.<br />
+                {{ t("Imprint.disclaimertext") }}
+                <br />
               </p>
             </div>
           </div>
           <div class="mr-8 tw-flex tw-flex-col">
             <div class="tw-flex tw-flex-col tw-gap-4">
               <p class="tw-text-3xl tw-font-extrabold heading_font tx-weiss">
-                Haftung für Links
+                {{ t("Imprint.liability") }}
               </p>
               <p class="tw-text-lg tx-weiss">
-                Unser Angebot enthält Links zu externen Webseiten Dritter, auf
-                deren Inhalte wir keinen Einfluss haben. Deshalb können wir für
-                diese fremden Inhalte auch keine Gewähr übernehmen. Für die
-                Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter
-                oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten
-                wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
-                überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der
-                Verlinkung nicht erkennbar. Eine permanente inhaltliche
-                Kontrolle der verlinkten Seiten ist jedoch ohne konkrete
-                Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
-                Bekanntwerden von Rechtsverletzungen werden.<br /><br />
+                {{ t("Imprint.liabilitytext") }}
+                <br /><br />
               </p>
             </div>
             <div class="mr-8 tw-flex tw-flex-col tw-gap-4">
               <p class="tw-text-3xl tw-font-extrabold heading_font tx-weiss">
-                Urheberrechte
+                {{ t("Imprint.copyright") }}
               </p>
               <p class="tw-text-lg tx-weiss">
-                Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
-                diesen Seiten unterliegen dem deutschen Urheberrecht. Die
-                Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
-                Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der
-                schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers.
-                Downloads und Kopien dieser Seite sind nur für den privaten,
-                nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf
-                dieser Seite nicht vom Betreiber erstellt wurden, werden die
-                Urheberrechte Dritter beachtet. Insbesondere werden Inhalte
-                Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
-                Urheberrechtsverletzung aufmerksam werden, bitten wir um einen
-                entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen
-                werden wir derartige Inhalte umgehend entfernen.<br />
+                {{ t("Imprint.copyrighttext") }}
+                <br />
               </p>
             </div>
           </div>
@@ -122,7 +93,19 @@
 </template>
 
 <script>
-export default {};
+import { useI18n } from "vue-i18n";
+
+export default {
+  name: "Imprint",
+
+  setup() {
+    const { t, locale } = useI18n({
+      inheritLocale: true,
+      useScope: "global",
+    });
+    return { t, locale };
+  },
+};
 </script>
 
 <style></style>
